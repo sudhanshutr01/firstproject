16b2aeb76ea0ce3fc70e5fa9ec1bb38a837e29ca
sudhanshutr01 <95619323+sudhanshutr01@users.noreply.github.com>
2021-12-06 15:57:11 +0530

    Add files via upload
    first github project

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,40,40);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW)){
+     box.position.x = box.position.x+5;
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x-5;
+ }
+
+  drawSprites();
+}
+
+
+
+
